0e352b6823a06c47adb09cc7fa9df10f96662bdb
piliponful <piliponful@gmail.com>
2017-06-25 21:05:54 +0300

    Added freactal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { provideState, injectState } from 'freactal';
 
-export default () => (
-  <h1>React Webpack 2 starter</h1>
-);
+const stateTemplate = provideState({
+  initialState: () => ({ version: '2', text: '' }),
+  effects: {
+    upgrade: () => state => ({ ...state, version: state.text }),
+    setText: (effects, text) => state => ({ ...state, text }),
+  },
+});
+
+export default stateTemplate(injectState(({
+  state: {
+    version,
+    text,
+  },
+  effects: {
+    upgrade,
+    setText,
+  },
+}) => (
+  <div>
+    <h1>React Webpack {version} starter</h1>
+    <button onClick={upgrade}>Upgrade</button>
+    <input value={text} onChange={e => setText(e.target.value)} />
+  </div>
+)));
