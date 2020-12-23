c40adb69d139183775db3c58639add9219f13b43
piliponful <piliponful@gmail.com>
2020-12-23 11:58:18 +0200

    add ability to filter messages by group #19

--- src/components/Root/components/GroupList/GroupList.js
+++ src/components/Root/components/GroupList/GroupList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useLocalStorage } from '@rehooks/local-storage'
 
-import { GET_GROUPS } from '../../../../srpcFunctionNames'
+import { GET_GROUPS, GET_MESSAGES } from '../../../../srpcFunctionNames'
 
 export const GroupList = () => {
   const [groups, setGroups] = useState([])
+  const [selectedGroupId, setSelectedGroupId] = useState(null)
+
+  const dispatch = useDispatch()
 
   const [jwt] = useLocalStorage('jwt')
 
@@ -25,11 +28,30 @@ export const GroupList = () => {
       })
   }, [])
 
-  const hideGroupList = useSelector(state => state.components.hideGroupList)
+  useEffect(() => {
+    window.fetch(CONFIG.apiUrl, {
+      method: 'post',
+      body: JSON.stringify({ method: GET_MESSAGES, params: { jwt, groupId: selectedGroupId } }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.error) {
+          throw new Error(result.error)
+        }
 
-  if (hideGroupList) {
-    return null
-  }
+        dispatch({ type: `${GET_MESSAGES}Result`, payload: { messages: result.messages } })
+      })
+  }, [selectedGroupId])
 
-  return <ul>{groups.map(group => group.name)}</ul>
+  return <ul>
+    {
+      groups.map(group => (
+        <span key={group.id}>
+          {group.name}
+          <button onClick={() => setSelectedGroupId(group.id)}>Filter By This Group</button>
+        </span>
+      ))
+    }
+  </ul>
 }
