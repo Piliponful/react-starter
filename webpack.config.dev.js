880f9572945afb7ee8c0f16768e19b18c4b5450d
piliponful <piliponful@gmail.com>
2021-04-03 11:22:21 +0300

    remove configjs #37

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 // const CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
-const config = require('config')
 
 const srcDir = path.resolve(__dirname, 'src')
 const distDir = path.resolve(__dirname, 'dist')
@@ -88,9 +86,6 @@ module.exports = {
     // new CopyWebpackPlugin([
     //   { from: 'src/assets/images/favicon.ico', to: distDir }
     // ]),
-    new webpack.DefinePlugin({
-      'CONFIG': JSON.stringify(config)
-    }),
     new ReactRefreshWebpackPlugin()
   ]
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
-const config = require('config')
 
 const srcDir = path.resolve(__dirname, 'src')
 const distDir = path.resolve(__dirname, 'dist')
@@ -69,9 +67,6 @@ module.exports = {
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
     new CopyWebpackPlugin([
       // { from: 'src/app/images/favicon.ico', to: distDir }
-    ]),
-    new webpack.DefinePlugin({
-      'CONFIG': JSON.stringify(config)
-    })
+    ])
   ]
 }
