4d52671e511345fe6be43c16475a5ed9adf8843f
piliponful <piliponful@gmail.com>
2021-04-09 12:06:31 +0300

    implement combination type validation #44

--- src/components/Root/components/AuthorizedScreen/components/GroupComposition/GroupComposition.js
+++ src/components/Root/components/AuthorizedScreen/components/GroupComposition/GroupComposition.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import projectXUI from 'project-x-ui'
-import { isEqual } from 'lodash'
 
 import { SET_COMPOSITION_TYPE } from '../../../../../../actions/compositionType'
 
 const { leafs: { Circles } } = projectXUI
-
-const circlePartsToCompositionType = circleParts => {
-  if (isEqual(circleParts, ['intersection'])) {
-    return 'intersection'
-  }
-  if (isEqual(circleParts, ['left-wing'])) {
-    return 'difference'
-  }
-  if (isEqual(circleParts, ['left-wing', 'intersection', 'right-wing'])) {
-    return 'union'
-  }
-}
 
 export const GroupComposition = () => {
   const selectedForComposition = useSelector(state => state.selectedForCompositionGroupIds)
@@ -27,13 +14,13 @@ export const GroupComposition = () => {
     return null
   }
 
-  const setCircleParts = value =>
-    dispatch({ type: SET_COMPOSITION_TYPE, payload: circlePartsToCompositionType(value) })
+  const setCompositionType = compositionType =>
+    dispatch({ type: SET_COMPOSITION_TYPE, payload: compositionType })
 
   return (
     <Circles
       selectedGroups={selectedForComposition}
-      onSelect={setCircleParts}
+      handleCompositionTypeChange={setCompositionType}
     />
   )
 }
