aff44606dcf6674af1877e68fa7c731d8316a4f2
piliponful <piliponful@gmail.com>
2021-04-21 16:16:48 +0300

    show username #57

--- src/components/Root/components/AuthorizedScreen/components/ActionsPanel/ActionsPanel.js
+++ src/components/Root/components/AuthorizedScreen/components/ActionsPanel/ActionsPanel.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import projectXUI from 'project-x-ui'
 import { useLocalStorage } from '@rehooks/local-storage'
+import decodeJwt from 'jwt-decode'
 
 const { leafs: { ActionsPanel: ActionsPanelUI } } = projectXUI
 
 export const ActionsPanel = () => {
-  const [_, _1, removeJwt] = useLocalStorage('jwt') // eslint-disable-line
+  const [jwt,, removeJwt] = useLocalStorage('jwt') // eslint-disable-line
+
+  const { username } = decodeJwt(jwt)
 
   const signOut = () => removeJwt()
 
   return (
-    <ActionsPanelUI logout={signOut} />
+    <ActionsPanelUI logout={signOut} username={username} />
   )
 }
