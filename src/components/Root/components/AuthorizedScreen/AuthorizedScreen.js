60690c88c1877bab6a42ac328a4e46abe5fc1ea7
piliponful <piliponful@gmail.com>
2021-05-13 17:05:09 +0300

    implement main screen responsiveness #58

--- src/components/Root/components/AuthorizedScreen/AuthorizedScreen.js
+++ src/components/Root/components/AuthorizedScreen/AuthorizedScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import projectXUI from 'project-x-ui'
 import { useLocalStorage } from '@rehooks/local-storage'
 
@@ -8,7 +8,38 @@ import GroupList from './components/GroupList'
 import GroupComposition from './components/GroupComposition'
 import ActionsPanel from './components/ActionsPanel'
 
-const { shallow: { Sidebar, MainScreen, QuestionCardsRow, GroupsContainer } } = projectXUI
+const { shallow: { Sidebar, MainScreen, QuestionCardsRow, GroupsContainer }, context: { MainScreenSwipeContext } } = projectXUI
+
+const MainScreenWithQuestions = () => (
+  <MainScreen>
+    <QuestionCardsRow title='Most answered'>
+      <MessageList messageColumn='mostAnswered' />
+    </QuestionCardsRow>
+    <QuestionCardsRow title='Unanimous'>
+      <MessageList messageColumn='unanimous' />
+    </QuestionCardsRow>
+    <QuestionCardsRow title='Latest'>
+      <MessageList messageColumn='latest' />
+    </QuestionCardsRow>
+  </MainScreen>
+)
+
+const SidebarWithGroups = () => (
+  <Sidebar title='Groups'>
+    <GroupComposition />
+    <GroupsContainer>
+      <GroupList />
+    </GroupsContainer>
+    <MessageInput />
+    <ActionsPanel />
+  </Sidebar>
+)
+
+const SidebarWithQuestions = () => (
+  <Sidebar title='Questions'>
+    <MessageList messageColumn='mostAnswered' />
+  </Sidebar>
+)
 
 export const AuthorizedScreen = () => {
   const [jwt] = useLocalStorage('jwt')
@@ -17,27 +48,12 @@ export const AuthorizedScreen = () => {
     return null
   }
 
+  const { mainScreen } = useContext(MainScreenSwipeContext)
+
   return (
     <>
-      <Sidebar>
-        <GroupComposition />
-        <GroupsContainer>
-          <GroupList />
-        </GroupsContainer>
-        <MessageInput />
-        <ActionsPanel />
-      </Sidebar>
-      <MainScreen>
-        <QuestionCardsRow title='Most answered'>
-          <MessageList messageColumn='mostAnswered' />
-        </QuestionCardsRow>
-        <QuestionCardsRow title='Unanimous'>
-          <MessageList messageColumn='unanimous' />
-        </QuestionCardsRow>
-        <QuestionCardsRow title='Latest'>
-          <MessageList messageColumn='latest' />
-        </QuestionCardsRow>
-      </MainScreen>
+      {mainScreen ? <SidebarWithGroups /> : <SidebarWithQuestions />}
+      {mainScreen ? <MainScreenWithQuestions /> : null}
     </>
   )
 }
